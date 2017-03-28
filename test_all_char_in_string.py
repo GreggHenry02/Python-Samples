05f3bf184fd0c48ad5aee7830546a7a505e3426c
GreggHenry02 <gregghenry+github@gmail.com>
2017-03-27 21:33:50 -0400

    Delete test_all_char_in_string.py

--- test_all_char_in_string.py
+++
@@ -1,7 +0,0 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-s = raw_input()
-print any(x.isalnum() for x in s)
-print any(x.isalpha() for x in s)
-print any(x.isdigit() for x in s)
-print any(x.islower() for x in s)
-print any(x.isupper() for x in s)
