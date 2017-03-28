ff8da7691b9df0ef91bfea817cb58a5a70d0e6f3
GreggHenry02 <gregghenry+github@gmail.com>
2017-03-27 21:32:57 -0400

    Delete print_variable.py

--- print_variable.py
+++
@@ -1,7 +0,0 @@
-	# Add your code here
-    def computeDifference(self):
-        min = self.__elements.__len__();
-        max = -1;
-        print "computerDifference, %d %d" % (min,max)
-        
-    maximumDifference = 12
